b880a90ff93f923b7f7ab6464f204c8907a58538
suntaehun <sth7862@naver.com>
2022-11-17 17:49:32 +0900

    mysite03 guestbook

--- mysite03/src/main/java/com/bitacademy/mysite/repository/UserRepository.java
+++ mysite03/src/main/java/com/bitacademy/mysite/repository/UserRepository.java
@@ -1,84 +1,29 @@
 package com.bitacademy.mysite.repository;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
-
-import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.bitacademy.mysite.exception.UserRepositoryException;
 import com.bitacademy.mysite.vo.UserVo;
 
 @Repository
 public class UserRepository {
-	@Autowired
-	private DataSource dataSource;
 	
 	@Autowired
 	private SqlSession sqlSession;
 	
 	public boolean update(UserVo vo) {
-		boolean result = false;
-
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-
-		try {
-			conn = dataSource.getConnection();
-			
-			if("".equals(vo.getPassword())) {
-				String sql = "update user set name=?, gender=? where no=?";
-				pstmt = conn.prepareStatement(sql);
-				
-				pstmt.setString(1, vo.getName());
-				pstmt.setString(2, vo.getGender());
-				pstmt.setLong(3, vo.getNo());
-			} else {
-				String sql = "update user set name=?, password=?, gender=? where no=?";
-				pstmt = conn.prepareStatement(sql);
-				
-				pstmt.setString(1, vo.getName());
-				pstmt.setString(2, vo.getPassword());
-				pstmt.setString(3, vo.getGender());
-				pstmt.setLong(4, vo.getNo());
-				
-			}
-
-			int count = pstmt.executeUpdate();
-
-			result = count == 1;
-
-		} catch (SQLException e) {
-			System.out.println("Error:" + e);
-		} finally {
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-
-		return result;
-
+		int count = sqlSession.update("user.update", vo);
+		return count == 1;
 	}
 	public UserVo findByNo(Long no) {
 		return sqlSession.selectOne("user.findByNo", no);
 	}
 
-	public UserVo findByEmailAndPassword(String email, String password) throws UserRepositoryException {
+	public UserVo findByEmailAndPassword(String email, String password) {
 		Map<String, Object> map = new HashMap<>();
 		map.put("email", email);
 		map.put("password", password);
