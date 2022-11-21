8593eb3f0951c3a2cd4ea9469dd7ac93d2bf428b
suntaehun <sth7862@naver.com>
2022-11-21 11:52:49 +0900

    logback

--- mysite03/src/main/java/com/bitacademy/mysite/controller/MainController.java
+++ mysite03/src/main/java/com/bitacademy/mysite/controller/MainController.java
@@ -1,13 +1,19 @@
 package com.bitacademy.mysite.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.bitacademy.mysite.exception.ControllerExceptionHandler;
+
 @Controller
 public class MainController {
+	private static final Log Logger = LogFactory.getLog(MainController.class);
 	
 	@RequestMapping({"", "/main"})
 	public String index() {
+		Logger.debug("MainController.index() called");
 		return "main/index";
 	}
 }
