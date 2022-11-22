7afe942cf494afc91f0df95c6135705ca38f8105
suntaehun <sth7862@naver.com>
2022-11-22 09:55:23 +0900

    bean Validation 1

--- mysite03/src/main/java/com/bitacademy/mysite/controller/UserController.java
+++ mysite03/src/main/java/com/bitacademy/mysite/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.bitacademy.mysite.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -24,7 +30,16 @@ public class UserController {
 	}
 	
 	@RequestMapping(value="/join", method=RequestMethod.POST)
-	public String join(UserVo userVo) {
+	public String join(@Valid UserVo userVo, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+//			List<ObjectError> errors = result.getAllErrors();
+//			for(ObjectError error : errors) {
+//				System.out.println(error);
+//			}
+			
+			model.addAllAttributes(result.getModel());
+			return "user/join";
+		}
 		userService.join(userVo);
 		return "redirect:/user/joinsuccess";
 	}
--- mysite03/src/main/webapp/WEB-INF/views/user/join.jsp
+++ mysite03/src/main/webapp/WEB-INF/views/user/join.jsp
@@ -2,6 +2,7 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -18,6 +19,13 @@
 				<form id="join-form" name="joinForm" method="post" action="${pageContext.request.contextPath }/user/join">
 					<label class="block-label" for="name">이름</label>
 					<input id="name" name="name" type="text" value="">
+					<p style="text-align: left; padding: 2px 0 2px 0; color: red">
+					<spring:hasBindErrors name="userVo">
+						<c:if test="${errors.hasFieldErrors('name') }">
+							<strong>${errors.getFieldError('name').defaultMessage}</strong>
+						</c:if>
+					</spring:hasBindErrors>
+					</p>
 
 					<label class="block-label" for="email">이메일</label>
 					<input id="email" name="email" type="text" value="">
