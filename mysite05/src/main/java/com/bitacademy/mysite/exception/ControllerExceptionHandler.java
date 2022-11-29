95280bddd14a7f8015473577472742fc678ea93a
suntaehun <sth7862@naver.com>
2022-11-29 14:12:44 +0900

    yml

--- mysite05/src/main/java/com/bitacademy/mysite/exception/ControllerExceptionHandler.java
+++ mysite05/src/main/java/com/bitacademy/mysite/exception/ControllerExceptionHandler.java
@@ -8,6 +8,7 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
@@ -15,13 +16,19 @@ public class ControllerExceptionHandler {
 	
 	@ExceptionHandler(Exception.class)
 	public String HandlerException(Model model, Exception e) {
-		// 로깅
+		// 404 Error 처리
+		if(e instanceof NoHandlerFoundException) {
+			return "error/404";
+		}
+		
+		// 500 Error 처리
+		// 1. 로깅
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
 //		System.out.println(errors.toString());
 		Logger.error(errors.toString());
 		
-		// 사과 페이지(HTML 응답, 정상종료)
+		// 2. 사과 페이지(HTML 응답, 정상종료)
 		model.addAttribute("exception", errors.toString());
 		return "error/exception";
 	}
